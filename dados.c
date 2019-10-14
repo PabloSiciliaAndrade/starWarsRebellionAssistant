36fec673ebba6bca65390c1869a62252d8fcd050
PabloSiciliaAndrade <52049994+PabloSiciliaAndrade@users.noreply.github.com>
2019-10-14 14:17:32 -0500

    combate

--- dados.c
+++ dados.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int TirarDadosMision(int NumeroDeDados, int puntos);
-int TirarDadosCombate(int DadosRojos, int DadosNegros);
+int TirarDadosCombate(int Dados);
+int combate();
+
 int Op,i;
 int DadosRojosAtacante, DadosNegrosAtacante, DadosAtacante, PuntosAtacante;
 int DadosRojosDefensor, DadosNegrosDefensor, DadosDefensor, PuntosDefensor;
@@ -27,31 +29,67 @@ int main(int argc, char const *argv[])
 	}else{
 		if (Op == 2)
 		{
-			do{
 			printf("inicio de ronda espacial\n");
 			system("PAUSE");
-			printf("dados rojos del atacante:\n");
-			scanf("%d",&DadosRojosAtacante);
-			printf("dados negros del atacante:\n");
-			scanf("%d",&DadosNegrosAtacante);
-
-			printf("dados rojos del defensor:\n");
-			scanf("%d",&DadosRojosDefensor);
-			printf("dados negros del defensor:\n");
-			scanf("%d",&DadosNegrosDefensor);
+			combate();
 
 			printf("inicio de ronda terrestre\n");
 			system("PAUSE");
-			
-
-			}while();
+			combate();
 
 		}
 	}
 	return 0;
 }
+int combate(){
+	printf("dados rojos y negros del atacante: \n");
+	scanf("%d %d",&DadosRojosAtacante, &DadosNegrosAtacante);
+
+	printf("dados rojos del defensor:\n");
+	scanf("%d",&DadosRojosDefensor);
+	printf("dados negros del defensor:\n");
+	scanf("%d",&DadosNegrosDefensor);
 
 
+	printf("los dados rojos del atacante fueron:\n");
+	TirarDadosCombate(DadosRojosAtacante);
+	printf("los dados negros del atacante fueron:\n");
+	TirarDadosCombate( DadosNegrosAtacante);
+
+	printf("los dados rojos del defensor fueron:\n");
+	TirarDadosCombate( DadosRojosDefensor);
+	printf("los dados negros del defensor fueron:\n");
+	TirarDadosCombate( DadosNegrosDefensor);
+
+
+}
+
+int TirarDadosCombate(int Dados){
+	for ( i = 0; i < Dados; ++i){
+		int DiceResult = rand() % 6;
+		switch(DiceResult){
+			case 0:
+				printf("fallo \n");
+				break;
+			case 1:
+				printf("fallo \n");
+				break;
+			case 2:
+				printf("daño \n");
+				break;
+			case 3:
+				printf("daño \n");
+				break;
+			case 4:
+				printf("impacto directo \n");
+				break;
+			case 5:
+				printf("especial \n");
+				break;
+		}
+	}
+	return 0;	
+}
 
 
 
